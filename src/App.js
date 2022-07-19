f7fce4d7547d1b51930391f492352937b95ede8c
Valentin Carmignac <carvallegro@users.noreply.github.com>
2022-07-19 14:51:16 -0700

    feat: changed style and content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={janeLogo} className="App-logo" alt="logo" />
         <h1>
-          Jane has a pink background
+          Jane has a red background
         </h1>
       </header>
     </div>
