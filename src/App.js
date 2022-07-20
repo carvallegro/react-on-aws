db60cf34b2e37fa6b89b80278c864cbef33a3ddb
Valentin Carmignac <carvallegro@users.noreply.github.com>
2022-07-20 08:50:56 -0700

    feat: changed to red

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={janeLogo} className="App-logo" alt="logo" />
         <h1>
-          Jane has a red background
+          Jane has a GREEN background
         </h1>
       </header>
     </div>
