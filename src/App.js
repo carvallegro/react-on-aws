51922a0815031d8ed91ac79c1646d6135ff11629
Valentin Carmignac <carvallegro@users.noreply.github.com>
2022-07-21 10:19:29 -0700

    chore: cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import janeLogo from './jane_logo_200.png'
 import './App.css';
 
+const VERSION_NUMBER = process.env.REACT_APP_VERSION
+
 function App() {
+    console.log("Version number", VERSION_NUMBER)
   return (
     <div className="App">
       <header className="App-header">
         <img src={janeLogo} className="App-logo" alt="logo" />
         <h1>
-          Jane has a GREEN background
+          Jane has a BLUE background
         </h1>
+          <h2>Version number: {VERSION_NUMBER}</h2>
       </header>
     </div>
   );
